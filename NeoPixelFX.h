60a7e779d967992b999f99d5a3c5178db64d3afd
Stefan Nuernberger <kabelfrickler@gmail.com>
2017-12-24 21:40:45 +0100

    NeoPixelFX: add average compose function
    Signed-off-by: Stefan Nuernberger <kabelfrickler@gmail.com>

--- NeoPixelFX.h
+++ NeoPixelFX.h
@@ -10,24 +10,26 @@
 #ifndef NEOPIXELFX_H
 #define NEOPIXELFX_H
 
+// FIXME: Not all modifiers/generators listed below are implemented.
+
 // Choose any modifiers:
 // ---------------------
 // confined
+// offset
 // mirrored
 // moving
 // blinking
-// pulsating (brightness)
-// rotating (color channels)
-// expanding
-// shrinking / contracting
-// dripping
-// bouncing
+// pulsing (intensity)
+// decaying
 
 // Choose one generator:
 // ---------------------
+// CurrentColor
 // RandomColor
 // ConstantColor
+// ConstantRGBColor
 // SineWave
+// Rainbow
 // Pattern
 
 // Choose one composer function:
@@ -36,7 +38,7 @@
 // compose_or
 // compose_and
 // compose_cover
-
+// compose_avg
 
 // Define composer functions.
 
@@ -54,6 +56,16 @@ const uint32_t compose_and(const uint32_t first, const uint32_t second) {
 
 const uint32_t compose_cover(const uint32_t first, const uint32_t second) {
   return second ? second : first;
+}
+
+const uint32_t compose_avg(const uint32_t first, const uint32_t second) {
+  // FIXME: I am pretty sure there is a nicer bit-twiddling hack for this.
+  // Compute average of channels 1 and 3
+  uint32_t avg13 = (((first & 0x00ff00ff) + (second & 0x00ff00ff)) >> 1) & 0x00ff00ff;
+  // Compute average of channels 2 and 4
+  uint32_t avg24 = (((first >> 8) & 0x00ff00ff) + ((second >> 8) & 0x00ff00ff) >> 1) & 0x00ff00ff;
+  // Return all channels average
+  return avg13 | (avg24 << 8);
 }
 
 // Provide a convenience function pointer name for composer functions.
