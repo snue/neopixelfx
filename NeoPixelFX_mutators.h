f29973af7733b39ae49ba95ce874c4e0e83d1df4
Stefan Nuernberger <kabelfrickler@gmail.com>
2018-07-22 21:28:00 +0200

    Add more mutators.
     - fixed (position)
     - blinking
     - constant decay
    
    Signed-off-by: Stefan Nuernberger <kabelfrickler@gmail.com>

--- NeoPixelFX_mutators.h
+++ NeoPixelFX_mutators.h
@@ -51,6 +51,25 @@ public:
   }
 };
 
+template<int16_t FIXED, typename FX>
+class fixed {
+  const Adafruit_NeoPixel &pixels;
+  FX chain_fx;
+
+public:
+  fixed(const Adafruit_NeoPixel &pixel)
+    : pixels(pixel), chain_fx(pixel)
+  {}
+
+  uint32_t getPixelColor(uint16_t pos) const {
+    return chain_fx.getPixelColor(FIXED);
+  }
+
+  void update() {
+    chain_fx.update();
+  }
+};
+
 template<typename FX>
 class mirrored {
   const Adafruit_NeoPixel &pixels;
@@ -127,6 +146,62 @@ public:
   }
 
   void update() {
+    chain_fx.update();
+  }
+};
+
+template<uint8_t AMOUNT, typename FX>
+class constant_decaying {
+  FX chain_fx;
+
+public:
+  constant_decaying(const Adafruit_NeoPixel &pixel)
+    : chain_fx(pixel)
+  {}
+
+  uint32_t getPixelColor(uint16_t pos) const {
+    uint32_t color = chain_fx.getPixelColor(pos);
+    uint32_t result = 0;
+    uint32_t c1 = color & 0xff;
+    result |= c1 > AMOUNT ? (c1 - AMOUNT) << 0 : 0;
+    uint32_t c2 = (color >>  8) & 0xff;
+    result |= c2 > AMOUNT ? (c2 - AMOUNT) << 8 : 0;
+    uint32_t c3 = (color >> 16) & 0xff;
+    result |= c3 > AMOUNT ? (c3 - AMOUNT) << 16 : 0;
+    uint32_t c4 = (color >> 24) & 0xff;
+    result |= c4 > AMOUNT ? (c4 - AMOUNT) << 24 : 0;
+    return result;
+  }
+
+  void update() {
+    chain_fx.update();
+  }
+};
+
+template<uint16_t PERIOD, typename FX>
+class blinking {
+  FX chain_fx;
+  uint16_t period;
+  bool on;
+
+ public:
+  blinking(const Adafruit_NeoPixel &pixel)
+   : chain_fx(pixel), period(PERIOD), on(true)
+  {}
+
+  uint32_t getPixelColor(uint16_t pos) const {
+    if (on) {
+      return chain_fx.getPixelColor(pos);
+    } else {
+      return 0;
+    }
+  }
+
+  void update() {
+    if (not period--) {
+      on ^= 0x1;
+      period=PERIOD;
+    }
     chain_fx.update();
   }
 };
